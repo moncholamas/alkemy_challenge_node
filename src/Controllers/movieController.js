83ad5a8c9bed62c413d8e1fb6e07467264458eea
Manuel Lamas <moncholamas@gmail.com>
2021-10-19 17:09:33 -0300

    agregada busqueda por subcadena del titulo para pelicula_serie

--- src/Controllers/movieController.js
+++ src/Controllers/movieController.js
@@ -1,22 +1,72 @@
 import peliculas_series from '../Models/peliculas_series';
 import initModels from '../Models/init-models';
-import {sequelize} from '../DB/connection'
+import {sequelize} from '../DB/connection';
+import {Op} from 'sequelize';
 
 export async function getAll(req,res){
-    try {
-        initModels(sequelize);
-        const listado = await peliculas_series.findAll({attributes:['imagen','titulo','fecha_creacion']});
+    //si no tiene parametros de busqueda devolvemos todas las series/peliculas
+    if(Object.keys(req.query).length===0){
+        try {
+            initModels(sequelize);
+            const listado = await peliculas_series.findAll({attributes:['imagen','titulo','fecha_creacion']});
+            return res.json({
+                data: listado
+            });
+        } catch (error) {
+            console.log(error);
+            return res.json({
+                msg: "error al obtener peliculas"
+            })
+        }
+    }
+    else{
+        //hace la busqueda por los parametros ingresados
+        //si recibe un nombre busca por este campo
+        const {titulo, genero, order} = req.query;
+
+        //si ingresan el nombre
+        if(titulo){
+            const resultado = await buscarPorTitulo(titulo);
+            if(resultado.length===0){return res.json({msg:'no se encontraron items con el titulo ingresado'})}
+
+            return res.json({
+                data: resultado
+            });
+        }
+
+        //QUEDA PENDIENTE 
+                //Filtrar por genero y permitir ordenacion
+                //refactorizar 
+
+        //si ingresan cualquier otro parametro de busqueda
         return res.json({
-            data: listado
-        });
-    } catch (error) {
-        console.log(error);
-        return res.json({
-            msg: "error al obtener peliculas"
+            msg: "no se reconoce el campo para realizar filtros, por favor ingrese nombre, peso, edad o pelicula/serie"  
         })
     }
+    
 }
 
+async function buscarPorTitulo(titulo){
+    initModels(sequelize);
+    const listadoPersonajes = await peliculas_series.findAll({where:{titulo: {[Op.substring]: titulo}}});
+    return listadoPersonajes;
+}
+
+export async function getById(req, res){
+    const {id} = req.params;
+    try {
+        initModels(sequelize);
+        const detalles = await peliculas_series.findByPk(id);
+        if(!detalles){return res.json({msg: "no se encontraron coincidencias"})}
+
+        return res.json({
+            data: detalles
+        })
+    } catch (error) {
+        console.log(error);
+        res.json({msg:"error al buscar por id de pelicula o serie"})
+    }
+}
 
 export async function newMovie(req,res){
     //por el momento crea la serie sin genero ni personajes que aparecen
--- src/Routes/movieRouter.js
+++ src/Routes/movieRouter.js
@@ -1,10 +1,11 @@
 import Express from 'express';
-import {getAll, newMovie, deleteMovie, updateMovie} from '../Controllers/movieController'
+import {getAll, newMovie, deleteMovie, updateMovie, getById} from '../Controllers/movieController'
 
 const router = Express.Router();
 
 
 router.get('/', getAll);
+router.get('/:id', getById);
 router.post('/', newMovie);
 router.delete('/delete/:id', deleteMovie);
 router.put('/update/:id', updateMovie);
